3d5fbddd5b7ea2e2b04c08ca3b6b5d7584b6a51d
Anis Patel <anispatel677@gmail.com>
2026-03-12 03:03:31 +0530

    Add gitignore

--- app/utils/imagekitupload.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-from imagekitio import ImageKit
-import os
-load_dotenv()
-imagekit = ImageKit(
-    private_key=os.getenv("PRIVATE_KEY"),
-    # public_key=os.getenv("PUBLIC_KEY"),
-    # url_endpoint=os.getenv("URL_ENDPOINT"),
-)
